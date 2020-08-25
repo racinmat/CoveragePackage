a9f74d3f041cd6a6b7822a5810191a53784e58ee
Matěj Račinský <matej.racinsky@avast.com>
2020-08-25 15:21:17 +0200

    fixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using CoveragePackage
 using Test
 
 @testset "CoveragePackage.jl" begin
-    e = CoveragePackage.KeyAsField(1, "2")
-    e2 = CoveragePackage.KeyAsField(1, "2")
+    e = CoveragePackage.ExtractKeyAsField(1, "2")
+    e2 = CoveragePackage.ExtractKeyAsField(1, "2")
 	@test hash(e) === hash(e2)
 	@test e == e2
 end
