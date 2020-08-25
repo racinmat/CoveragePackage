27ff6be38c9f13c99ce263bf797ba319bde2c06b
Matěj Račinský <matej.racinsky@avast.com>
2020-08-25 14:37:36 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.7

---
+++ src/CoveragePackage.jl
@@ -0,0 +1,5 @@
+module CoveragePackage
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CoveragePackage
+using Test
+
+@testset "CoveragePackage.jl" begin
+    # Write your tests here.
+end
